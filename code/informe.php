30a2f0361f8cbbfa14192b550b4c26aa66998cb5
vicenteoyarzun <163044338+vicenteoyarzun@users.noreply.github.com>
2025-06-24 21:03:07 -0400

    Update informe.php

--- code/informe.php
+++ code/informe.php
@@ -1,18 +1,43 @@
 <?php
 include("conexion.php");
 
-$query = mysqli_query($conn, "SELECT FECHA, MUERTEACT FROM JAULAINF WHERE JAULAID = 101");
+$jaulaid = 101;
+
+$sql = "SELECT FECHA, MUERTEACT FROM JAULAINF WHERE JAULAID = ? ORDER BY FECHA";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $jaulaid);
+$stmt->execute();
+$result = $stmt->get_result();
 
 $fechas = [];
 $muertes = [];
-
-while ($row = mysqli_fetch_assoc($query)) {
+while ($row = $result->fetch_assoc()) {
     $fechas[] = $row['FECHA'];
     $muertes[] = $row['MUERTEACT'];
 }
 
+$muerteActual = 0;
+if (count($muertes) >= 2) {
+    $muerteActual = $muertes[count($muertes) - 1] - $muertes[count($muertes) - 2];
+}
+
+$sql2 = "SELECT COSECHAINICIAL, MUERTEESP FROM JAULA WHERE JAULAID = ?";
+$stmt2 = $conn->prepare($sql2);
+$stmt2->bind_param("i", $jaulaid);
+$stmt2->execute();
+$result2 = $stmt2->get_result();
+$row2 = $result2->fetch_assoc();
+
+$cosecha = $row2['COSECHAINICIAL'];
+$muerteEsperada = $row2['MUERTEESP'];
+
+$porcentajeActual = $cosecha > 0 ? ($muerteActual / $cosecha) * 100 : 0;
+$porcentajeEsperado = $muerteEsperada;
+
 echo json_encode([
-    "labels" => $fechas,
-    "data" => $muertes
+    "fechas" => $fechas,
+    "muertes" => $muertes,
+    "porcentajeActual" => $porcentajeActual,
+    "porcentajeEsperado" => $porcentajeEsperado
 ]);
 ?>
